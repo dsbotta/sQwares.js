1f47b711e762cd92ce8bbc8b170dcdf1c25874d4
dsbotta <danielbotta@Daniels-MacBook-Pro.local>
2015-07-19 12:59:12 -0400

    Added new prototype functions and some new animation functionality to 4, 9, and 16

--- js/sqwares.js
+++ js/sqwares.js
@@ -1,28 +1,41 @@
-//Must use sq-hook to initialize
+/*********************
+		SQWARES
+*********************/
+
+
+//MasterSquare constructor that holds default settings
 function SquareMaster() {
+	this.hideSq = $('.sq-hide');
+	
 	this.defaultSettings = {
 		hook: 'sq-hook',
 		number: 9,
 		color: "black",
 		size: '50',
-		animHandler: 'click'
+		animHandler: 'click',
+		animationType: false
 	};
 };
 
 	//
-	//Create a Square Prototype
+	//Create the MAIN Square Constructor
+		//Calls on SquareMaster for defaults
 	//
 	function Square(object) {
 		SquareMaster.call(this);
 		this.settings = $.extend(this.defaultSettings, object);
-		this.settings.hook = $('#' + this.settings.hook);
+		this.hook = $('#' + this.settings.hook);
 	};
-
 	Square.prototype = Object.create(SquareMaster.prototype);
+
 
 	//
 	//Create Initial Color
 	//
+	//Values can be accepted in the form of:
+		//string
+		//integer
+		//array container ints for (rgb)
 	Square.prototype.createColor = function() {
 		var typeTest = jQuery.type(this.settings.color);
 
@@ -44,20 +57,26 @@ function SquareMaster() {
 	};
 
 	//
-	//Choose Square
+	//Build the Square
 	//
 	Square.prototype.buildSquare = function(num) {
-		var $hook = this.settings.hook;
+
+		//Variables for this function
+		var $hook = this.hook;
 		var size = this.settings.size + 'px';
 		var sqCssClass = 'sq-' + num; 
 		var classSelector = '.' + sqCssClass;
 		var animHandler;
+
+		//Choose if animations occurs on hover 
+		//or on click event
 		if (this.settings.animHandler == 'hover') {
 			animHandler = 'sq-hover';
 		} else {
 			animHandler = 'sq-' + this.settings.animHandler + '-off';
 		};
 
+		//Build the HTML node
 		var htmlNode = '<div class="sq-container ';
 		htmlNode += animHandler;
 		htmlNode += '">';
@@ -70,49 +89,131 @@ function SquareMaster() {
 		};
 
 		htmlNode += '</div>';
+
+		//Append the HTML Node
 		$hook.append(htmlNode);
+
+		//Give the squares some basic styles
 		$hook.children().css({'height': size, 'width': size});
-		$(classSelector).css('background-color', this.createColor());
+		$(this.hook).find(classSelector).css('background-color', this.createColor());
+
 		//Cal the animation type
-		this.animations(this.settings.animHandler);
+		this.eventHandler(this.settings.animHandler, this.settings.animationType);
 	};
 
 	//
 	//Create the Square Type
 	//
 	Square.prototype.createSquares = function() {
-		console.log(this.settings.number);
+
+		//Determine if the product value 
+		//for the amount of squares is valid
 		if (this.settings.number == 4 || 
 			this.settings.number == 9 || 
 			this.settings.number == 16 || 
 			this.settings.number == 25 ||
 			this.settings.number == 36 ||
-			this.settings.number == 49 ||
-			this.settings.number == 64) {
-
+			this.settings.number == 49) 
+		{
 				this.buildSquare(this.settings.number);
+				this.animationType(this.settings.animationType, this.settings.animHandler);
 
 		} else {
-			console.log("Error: Not a Valid Number Choice.");
+
+			//Tell the user if they did not enter a valid number
+			console.log('Error: ' + this.settings.number + ' is not a valid number choice. Please choose one of the follow:\n4\n9\n16\n25\n36\n49');
 			return null;
 		}
 	};
 
-	Square.prototype.animations = function(animType) {
+
+	//
+	//A function to Determine if animation occurs on click or not
+	//
+	Square.prototype.eventHandler = function(animType, type) {
 		if (animType == 'click') {
-			$('.sq-container').click(function() {
-				$('.sq-container').toggleClass('sq-click-on sq-click-off');
+			$(this.hook).children().click(function() {
+				$(this).toggleClass('sq-click-on sq-click-off');
+				if(type) {
+					//If animationType was specified:
+						//Create a click handler to toggle classes for the animation
+					$(this).children('.sq-' + type + '-click')
+						.toggleClass('sq-' + type + '-click-on', 'sq-' + type + '-click-off');
+				}
 			});
 		} else {
-
+			return null;
 		}
-	}
+	};
 
-//Initialize a squate instance
-var newSquare = new Square({
-	number: 16,
-	size: 300,
-	color: 'teal',
-	animHandler: 'click'
-});
-newSquare.createSquares();
+	Square.prototype.animationType = function(type, animHandler) {
+		//GET number of squares in the instance
+			//Store the length in the variable number
+		var number = $(this.hook).children().children().length;
+
+		//A function that returns the animHandler
+			//to be appended to the end of the class name
+			//This class name is used either for a click animation
+			//or a hover animation
+		function checkHandler() {
+			if (animHandler === 'click') {
+				return animHandler;
+			} else {
+				return animHandler;
+			}
+		};
+		var count = 0;
+
+		$(this.hook).children().children().each(function() {
+
+			if(type == "standard") {
+
+				//Class name prefix
+				var standard = 'sq-standard-';
+
+				//Determine animtion based on number of squares
+					//NOTE: number will show as -1 for index purposes
+				if(number === 4) {
+					
+					if( count > 0 && count < 3 ) {
+						$(this).addClass(standard + checkHandler());
+					}
+
+				} else if (number === 9) {
+
+					if ( (count % 2) !== 0 ) {
+						$(this).addClass(standard + checkHandler());
+					}
+
+				} else if (number === 16) {
+					function standard16() {
+						if ( (count % 3) === 0 ) {
+							return false;
+						}
+						if (count < 1) {
+							return false;
+						}
+						if ((count % 5) === 0) {
+							return false;
+						}
+						return true;
+					}
+					if ( standard16() ) {
+						$(this).addClass(standard + checkHandler());	
+					}
+				}
+
+			} else if(type == "smile") {
+
+				console.log('smile');
+
+			} else {
+
+				console.log('something');
+
+			}
+
+			count++;
+
+		});
+	};
